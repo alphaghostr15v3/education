829d13bb81579badf9a25b56a50706f90900e627
alphaghostr15v3 <alphaghostr15v3@gmail.com>
2026-02-05 18:42:45 +0530

    file changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
 </head>
 <body>
