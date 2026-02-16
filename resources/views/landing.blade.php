e13daf568518e390cbda7280ae0f361c55317288
alphaghostr15v3 <alphaghostr15v3@gmail.com>
2026-02-16 19:05:21 +0530

    ewifewofhewfhewo fwefh9ewfgoueuilafelf eipuafhughp89ergaer hyper

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,7 +5,7 @@
     <div class="container-fluid p-0">
         <div class="row g-0 align-items-stretch">
             <div class="col-lg-6 d-lg-flex align-items-lg-center">
-                <div class="hero-modern-content ps-lg-5 ms-lg-5 pe-lg-4">
+                <div class="hero-modern-content ps-lg-5 ms-lg-5 pe-lg-4 mt-lg-5 pt-lg-4">
                     <h1 class="hero-modern-title">Yadupati Singhania <br> Vocational Education <br> Foundation (YPSVEF)</h1>
                     <p class="hero-modern-description">
                         Building future-ready vocational skills that lead to real jobs, sustainable livelihoods, and industry relevance across India.
